cfeba82239f6b63752aaab7a7e0ff9d23c11077b
Leandro M. Nascimento <leandro.marques@gmail.com>
2016-10-07 16:29:29 -0300

    teste

---
+++ POO - Aula - Array_ArrarList/src/ufrpe/main/AppAnuncio.java
@@ -0,0 +1,14 @@
+package ufrpe.main;
+
+public class AppAnuncio {
+
+    public static void main(String[] parametros) {
+        if (parametros.length > 0) {
+            for(int i = 0; i < parametros.length; i++) {
+                System.out.println("Param " + i + ": " + parametros[i]);
+            }
+        }
+        System.out.println("Terminou");
+    }
+
+}
